cc9b532d8310f92ca82a67e1392fe86386a66b5e
MusaAqeel <77996753+MusaAqeel@users.noreply.github.com>
2022-12-03 23:55:38 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -62,10 +62,13 @@ class App extends Component {
             {courses.map((course) => (
               <div key={course.code}>
                 <h3>{course.name}</h3>
+                <p>{course.teacher}</p>
+                <p>{course.mark}</p>
                 <p>{course.code}</p>
                 <p>{course.teacher}</p>
                 <p>{course.mark}</p>
                 
+                <p>{course.overall_mark}</p>
                 <p>{course.overall_mark}</p>
               </div>
             ))}
