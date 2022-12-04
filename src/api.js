abd976ae7a1a4478fb131b638e5e32058951ef60
MusaAqeel <77996753+MusaAqeel@users.noreply.github.com>
2022-12-03 23:26:21 -0500

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,4 +1,4 @@
-const API_URL = 'https://api.pegasis.site/public/yrdsb_ta';
+const API_URL = 'https://api.pegasis.site/public/yrdsb_ta/getmark_v2';
 
 export default function getStudentGrades(username, password) {
   return fetch(`${API_URL}?username=${username}&password=${password}`)
