90746da06a0930799ddef834cee8374dcff10d1b
MusaAqeel <77996753+MusaAqeel@users.noreply.github.com>
2022-12-07 12:59:55 -0500

    spacing, block, period

--- src/App.js
+++ src/App.js
@@ -100,9 +100,8 @@ class App extends Component {
                   <p className="course-teacher" style={{color: "gray"}}>{course.teacher}</p>
                   <p className="course-mark" style={{color: "blue"}}>{course.mark}</p>
                   <p className="course-code" style={{fontStyle: "italic"}}>{course.code}</p>
-                  <p className="course-room" style={{fontWeight: "bold"}}>{"Room: " + course.room + " Block: " + course.block}</p>
-                  {/* Course Assignment Tester */}
-                  <p classname="course.assignment" style={{fontWeight: "bold"}}>{"A" + course.assignments.feedback}</p>
+                  <p className="course-room" style={{fontWeight: "bold"}}>{"Room: " + course.room + " | Block: " + course.block}</p>
+
                   {course.overall_mark ?
                     <p className="course-overall-mark" style={{fontWeight: "bold", fontFamily: 'Helvetica'}}>{`Overall Mark: ${course.overall_mark.toFixed(1)}%`}</p> :
                     <p className="course-overall-mark" style={{fontWeight: "bold", fontFamily: 'Helvetica'}}>{`Grade Not Open for ${course.name || "Course"}`}</p>
